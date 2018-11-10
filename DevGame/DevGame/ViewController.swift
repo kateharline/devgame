40460bdd5b3600399eb672ab3295898499175c3a
kateharline <harhark8@gmail.com>
2018-11-10 13:07:33 -0500

    first commit

---
+++ DevGame/DevGame/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  DevGame
+//
+//  Created by Kate Harline on 11/10/18.
+//  Copyright © 2018 Kate Harline. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var theLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
